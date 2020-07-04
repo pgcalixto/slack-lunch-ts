d79124dc64cdfce8cdd514f2994e26ede0945bc0
Pedro Calixto <pgcalixto@gmail.com>
2020-07-04 12:03:35 -0300

    Transform app to API
    Transform the app to an API, so that it can be remotely activated.

--- index.ts
+++ index.ts
@@ -1,130 +1,18 @@
-import { WebClient, WebAPICallResult } from "@slack/web-api";
+import * as restify from "restify"; // TODO: include @types/restify
+import { beginLunch } from "./lib/lunch";
 
-const SECOND = 1000;
-const MINUTE = 60 * SECOND;
-const HOUR = 60 * MINUTE;
+function lunch(req, res, next) {
+  beginLunch(); // TODO: await?
 
-const channel = "#canal-pessoal-calixto";
-const lunchMessage = "vou almoçar! 🍛";
-const finishLunchMessage = "voltei do almoço";
-const finishLunchMessageDate = Math.round(
-  (new Date().getTime() + 1 * HOUR) / 1000
-);
+  res.send('ok!');
 
-const resumeWorkMessage = "voltar a trabalhar!";
-const resumeWorkMessageDate = Math.round(
-  (new Date().getTime() + 1 * HOUR - 5 * MINUTE) / 1000
-);
-const resumeWorkMessageDeleteTimeout =
-  finishLunchMessageDate * 1000 - new Date().getTime() + 10 * MINUTE;
-
-const lunchStatusMessage = "almoçando";
-const lunchStatusEmoji = "🍛";
-const lunchStatusExpirationDate = Math.round(
-  (new Date().getTime() + 1 * HOUR - 10 * MINUTE) / 1000
-);
-
-interface AddReminderResponse extends WebAPICallResult {
-  ok: boolean,
-  reminder: {
-    id: string,
-    creator?: string,
-    user?: string,
-    text?: string,
-    recurring?: boolean,
-    time?: number,
-    complete_ts?: number
-  }
+  next();
 }
 
-async function addReminder(
-  slackWebClient: WebClient,
-  message: string,
-  date: number
-) {
-  return slackWebClient.reminders.add({
-    text: message,
-    time: date
-  });
-}
+const server = restify.createServer();
 
-async function deleteReminder(slackWebClient: WebClient, id: string) {
-  return slackWebClient.reminders.delete({ reminder: id });
-}
+server.post('/lunch', lunch);
 
-async function sendMessage(
-  slackWebClient: WebClient,
-  channel: string,
-  message: string
-) {
-  return slackWebClient.chat.postMessage({
-    channel: channel,
-    text: message
-  });
-}
-
-async function scheduleMessage(
-  slackWebClient: WebClient,
-  channel: string,
-  message: string,
-  date: number
-) {
-  return slackWebClient.chat.scheduleMessage({
-    channel: channel,
-    text: message,
-    post_at: date.toString()
-  });
-}
-
-async function setStatus(
-  slackWebClient: WebClient,
-  text: string,
-  emoji: string,
-  expirationDate: number
-) {
-  const status = {
-    status_text: text,
-    status_emoji: emoji,
-    status_expiration: expirationDate
-  };
-
-  return slackWebClient.users.profile.set({
-    profile: JSON.stringify(status)
-  });
-}
-
-async function main() {
-  const token = process.env.SLACK_TOKEN;
-
-  const slackWebClient = new WebClient(token);
-
-  sendMessage(slackWebClient, channel, lunchMessage);
-
-  setStatus(
-    slackWebClient,
-    lunchStatusMessage,
-    lunchStatusEmoji,
-    lunchStatusExpirationDate
-  );
-
-  scheduleMessage(
-    slackWebClient,
-    channel,
-    finishLunchMessage,
-    finishLunchMessageDate
-  );
-
-  const resumeWorkReminderResponse: AddReminderResponse = await addReminder(
-    slackWebClient,
-    resumeWorkMessage,
-    resumeWorkMessageDate
-  ) as AddReminderResponse;
-
-  const resumeWorkReminderId = resumeWorkReminderResponse.reminder.id;
-
-  setTimeout(() => {
-    deleteReminder(slackWebClient, resumeWorkReminderId);
-  }, resumeWorkMessageDeleteTimeout);
-}
-
-main();
+server.listen(5000, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
---
+++ lib/lunch.ts
@@ -0,0 +1,147 @@
+import { WebClient, WebAPICallResult } from "@slack/web-api";
+
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+
+// TODO: internationalize messages
+const channel = "#canal-pessoal-calixto";
+const lunchMessage = "vou almoçar! 🍛";
+const finishLunchMessage = "voltei do almoço";
+const finishLunchMessageDate = Math.round(
+  (new Date().getTime() + 1 * HOUR) / 1000
+);
+
+const resumeWorkMessage = "voltar a trabalhar!";
+const resumeWorkMessageDate = Math.round(
+  (new Date().getTime() + 1 * HOUR - 5 * MINUTE) / 1000
+);
+const resumeWorkMessageDeleteTimeout =
+  finishLunchMessageDate * 1000 - new Date().getTime() + 10 * MINUTE;
+
+const lunchStatusMessage = "almoçando";
+const lunchStatusEmoji = "🍛";
+const lunchStatusExpirationDate = Math.round(
+  (new Date().getTime() + 1 * HOUR - 10 * MINUTE) / 1000
+);
+
+interface AddReminderResponse extends WebAPICallResult {
+  ok: boolean,
+  reminder: {
+    id: string,
+    creator?: string,
+    user?: string,
+    text?: string,
+    recurring?: boolean,
+    time?: number,
+    complete_ts?: number
+  }
+}
+
+async function addReminder(
+  slackWebClient: WebClient,
+  message: string,
+  date: number
+) {
+  return slackWebClient.reminders.add({
+    text: message,
+    time: date
+  });
+}
+
+async function deleteReminder(slackWebClient: WebClient, id: string) {
+  return slackWebClient.reminders.delete({ reminder: id });
+}
+
+async function sendMessage(
+  slackWebClient: WebClient,
+  channel: string,
+  message: string
+) {
+  return slackWebClient.chat.postMessage({
+    channel: channel,
+    text: message
+  });
+}
+
+async function scheduleMessage(
+  slackWebClient: WebClient,
+  channel: string,
+  message: string,
+  date: number
+) {
+  return slackWebClient.chat.scheduleMessage({
+    channel: channel,
+    text: message,
+    post_at: date.toString()
+  });
+}
+
+async function setStatus(
+  slackWebClient: WebClient,
+  text: string,
+  emoji: string,
+  expirationDate: number
+) {
+  const status = {
+    status_text: text,
+    status_emoji: emoji,
+    status_expiration: expirationDate
+  };
+
+  return slackWebClient.users.profile.set({
+    profile: JSON.stringify(status)
+  });
+}
+
+let isLunching = false;
+
+async function beginLunch() {
+  if (isLunching) {
+    console.log("I am already lunching.");
+    return;
+  }
+
+  console.log("Start lunch.");
+  isLunching = true;
+
+  const token = process.env.SLACK_TOKEN;
+
+  const slackWebClient = new WebClient(token);
+
+  sendMessage(slackWebClient, channel, lunchMessage);
+
+  setStatus(
+    slackWebClient,
+    lunchStatusMessage,
+    lunchStatusEmoji,
+    lunchStatusExpirationDate
+  );
+
+  scheduleMessage(
+    slackWebClient,
+    channel,
+    finishLunchMessage,
+    finishLunchMessageDate
+  );
+
+  const resumeWorkReminderResponse: AddReminderResponse = await addReminder(
+    slackWebClient,
+    resumeWorkMessage,
+    resumeWorkMessageDate
+  ) as AddReminderResponse;
+
+  const resumeWorkReminderId = resumeWorkReminderResponse.reminder.id;
+
+  setTimeout(() => {
+    deleteReminder(slackWebClient, resumeWorkReminderId);
+  }, resumeWorkMessageDeleteTimeout);
+
+  setTimeout(() => {
+    isLunching = false
+  }, 1 * HOUR); // TODO: move timeout value to constant
+}
+
+export {
+  beginLunch
+};
